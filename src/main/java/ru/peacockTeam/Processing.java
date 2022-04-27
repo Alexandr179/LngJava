c4739a35b20cee9fe80a96b05071a14ea9c382e9
alexandr <orange179@mail.ru>
2022-04-28 02:16:23 +0300

    Feat: groupMap realisation with maps united by key

--- src/main/java/ru/peacockTeam/Processing.java
+++ src/main/java/ru/peacockTeam/Processing.java
@@ -1,130 +1,98 @@
 package ru.peacockTeam;
 
 import ru.peacockTeam.utils.FiosUtil;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import static ru.peacockTeam.utils.FiosUtil.getCurrentTime;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Processing {
 
     public static String SOURCE_LNG_FILE = "lng_test.csv";
-    public FiosUtil fiosUtil;
-    private Integer groupCounter = 0;
-    private Integer rowCounter = 0;
-    public volatile BufferedReader csvReader;
-    public byte[] resourceFileByteStream;
-
-
-    Map<String, Set<List<String>>> rowsMap = new HashMap<>();
-    Map<String, Set<String>> vertexMap = new HashMap<>();
-
+    private final FiosUtil fiosUtil;
+    private Integer groupCounter = 1;
+    public Map<String, Set<List<String>>> rowsMap = new HashMap<>();
+    public Map<String, Set<String>> interceptionsMap = new ConcurrentHashMap<>();
+    public List<List<String>> groupList = new LinkedList<>();
 
     public Processing() {
         this.fiosUtil = new FiosUtil();
     }
 
-
-//    public List<Map<String, List<List<String>>>> groupMapList = new ArrayList<>();
-
     public void process() throws IOException {
-        resourceFileByteStream = fiosUtil.getCopyByteArrayStream(fiosUtil.getResourceFileStream());
-        csvReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(resourceFileByteStream)));
-        System.out.println(">> API process. Building GroupMap...");
+        byte[] resourceFileByteStream = fiosUtil.getCopyByteArrayStream(fiosUtil.getResourceFileStream());
+        BufferedReader csvReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(resourceFileByteStream)));
+        System.out.println(">> API process. Building GroupMaps...");
         String rowAsStr;
         while ((rowAsStr = csvReader.readLine()) != null) {
             List<String> row = fiosUtil.getRowSet(rowAsStr);
-            if (!row.isEmpty()) buildGroupMap(row);
+            if (!row.isEmpty()) buildMaps(row);
         }
         csvReader.close();
+        collapseMaps();
 //        outputGroups();
     }
 
-    public void buildGroupMap(List<String> row) {
-        for(String rowItem : row){
+    public void buildMaps(List<String> row) {
+        for (String rowItem : row) {
             Set<String> vertexSet = new HashSet<>();
             Set<String> iterationRow = new HashSet<>(row);
             iterationRow.remove(rowItem);
             vertexSet.addAll(iterationRow);
-            vertexMap.merge(rowItem, vertexSet, (x, y) -> {
+            interceptionsMap.merge(rowItem, vertexSet, (x, y) -> {
+                x.addAll(y);
+                return x;
+            });
+            Set<List<String>> rowSet = new HashSet<>();
+            rowSet.add(row);
+            rowsMap.merge(rowItem, rowSet, (x, y) -> {
                 x.addAll(y);
                 return x;
             });
         }
+    }
 
-
-//        Set<String> vertexSet = new HashSet<>();
-//        for (int i = 1; i < row.size()-1; i++) {
-//            vertexSet.add(row.get(i));
-//        }
-//        vertexMap.putIfAbsent(row.get(0), vertexSet);
-//        vertexMap.merge(row.get(0), vertexSet, (x, y) -> {
-//            x.addAll(y);
-//            return x;
-//        });
-
-//        Set<String> vertexSet = new HashSet<>();
-//        vertexSet.addAll()
-//        for(String vertex : row){
-//        vertexMap.merge(vertex, vertexMap.get(vertex), (x, y) -> {
-//            x.addAll(y);
-//            return x;
-//        });
-//
-//        Set<List<String>> rowSet = new HashSet<>();
-//        rowsMap.merge(vertex, rowsMap.get(vertex), (x, y) -> {
-//            x.addAll(y);
-//            return x;
-//        });
-        }
-
-
-//        rowCounter++;
-//        Map<String, List<List<String>>> collapsedGroupMap = new HashMap<>();
-//        boolean intersection = false;
-//        for (String rowItem : row) {
-//            Iterator<Map<String, List<List<String>>>> groupMapListIterator = groupMapList.iterator();
-//            while (groupMapListIterator.hasNext()) {
-//                Map<String, List<List<String>>> groupMap = groupMapListIterator.next();
-//                if (groupMap.containsKey(rowItem)) {
-//                    groupMap.forEach(collapsedGroupMap::put);
-//                    putRowToGroupMap(row, collapsedGroupMap);
-//                    groupMapListIterator.remove();
-//                    intersection = true;
-//                }
-//            }
-//            if (intersection) groupMapList.add(collapsedGroupMap);
-////            System.err.println("DEBUG. FileRow<" + rowCounter + ">, total groups: " + groupMapList.size() + ". Time: " + getCurrentTime() + " [INTERSECTION]");
-//        }
-//        if (!intersection) putRowToNewGroupMap(row);
-//    }
-
-    private void putRowToGroupMap(List<String> row, Map<String, List<List<String>>> newGroupMap) {
-        for (String rowItem : row) {
-            List<List<String>> groupMapItemValue = new ArrayList<>();
-            groupMapItemValue.add(row);
-            newGroupMap.put(rowItem, groupMapItemValue);
+    private void collapseMaps() {
+        boolean isIntercept = false;
+        Iterator<Map.Entry<String, Set<String>>> interceptionsMapIterator = interceptionsMap.entrySet().iterator();
+        while (interceptionsMapIterator.hasNext()) {
+            Map.Entry<String, Set<String>> entryIntspMap = interceptionsMapIterator.next();
+            String key = entryIntspMap.getKey();
+            Iterator<String> entryIntcpMapValuesIterator = entryIntspMap.getValue().iterator();
+            while (entryIntcpMapValuesIterator.hasNext()) {
+                String iteratedValue = entryIntcpMapValuesIterator.next();
+                if (interceptionsMap.containsKey(iteratedValue)) {
+                    Set<String> collapseIntersections = interceptionsMap.get(key);
+                    interceptionsMap.merge(iteratedValue, collapseIntersections, (x, y) -> {
+                        x.addAll(y);
+                        return x;
+                    });
+                    Set<List<String>> collapseRows = rowsMap.get(key);
+                    rowsMap.merge(iteratedValue, collapseRows, (x, y) -> {
+                        x.addAll(y);
+                        return x;
+                    });
+                    isIntercept = true;
+                }
+            }
+            System.out.println(rowsMap.get(key));
+            interceptionsMap.remove(key);
+            rowsMap.remove(key);
         }
     }
 
-//    private void putRowToNewGroupMap(List<String> row) {
-//        Map<String, List<List<String>>> newGroupMap = new HashMap<>();
-//        putRowToGroupMap(row, newGroupMap);
-//        groupMapList.add(newGroupMap);
-////        System.out.println("DEBUG. FileRow<" + rowCounter + ">, total groups: " + groupMapList.size() + ". Time: " + getCurrentTime());
-//    }
-
-//    private void outputGroups() {
-//        System.out.println("\nAPI groups number: " + groupMapList.size());
-//        for (Map<String, List<List<String>>> groupMap : groupMapList) {
-//            System.out.println("\nGroup<" + groupCounter++ + ">:");
-//            groupMap.values().stream()
-//                    .sorted(Comparator.comparingInt(List::size))
-//                    .distinct().forEach(System.out::println);
-//        }
-//    }
+    private void outputGroups() {
+        for (Map.Entry<String, Set<List<String>>> entry : rowsMap.entrySet()) {
+            System.out.println("Group<" + groupCounter++ + ">:" +
+                    "\n---------------------------------------------------");
+            entry.getValue().stream()
+                    .sorted(Comparator.comparingInt(List::size))
+                    .distinct().forEach(System.out::println);
+        }
+    }
 }
 
 
